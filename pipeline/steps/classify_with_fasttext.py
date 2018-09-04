29e7c499ce5df753761130a7265b19dc790efa1c
sdg <sarah.diot-girard@people-doc.com>
2018-09-04 11:59:21 +0200

    Update classify step to store results with mlflow

--- pipeline/steps/classify_with_fasttext.py
+++ pipeline/steps/classify_with_fasttext.py
@@ -1,11 +1,12 @@
 # Generated from pipeline/notebooks/FastText.ipynb
-def fasttext(input_csv_file: str, model_path: str, learning_rate: float, epochs: int):
+def fasttext(input_csv_file: str, model_path: str, run_uuid_file: str, learning_rate: float, epochs: int):
     """
     :param str input_csv_file: Path to input file
     :param str model_path: Path to model files
+    :param str run_uuid_file: Path to file for storing run uuid
     :param float learning_rate: Learning rate
     :param int epochs: Number of epochs
-    :dvc-cmd: dvc run -d ./data/data_train_tokenized.csv  -o ./data/fasttext_model.bin ./scripts/python/classify --learning-rate 0.7 --epochs 10 --input-csv-file ./data/data_train_tokenized.csv  --model-path  ./data/fasttext_model 
+    :dvc-cmd: dvc run -d ./data/data_train_tokenized.csv  -o ./data/fasttext_model.bin -o ./data/run_uuid.txt ./scripts/python/classify --learning-rate 0.7 --epochs 10 --input-csv-file ./data/data_train_tokenized.csv  --model-path  ./data/fasttext_model --run-uuid-file  ./data/run_uuid.txt 
     """
 
     import pandas as pd
@@ -14,6 +15,7 @@ def fasttext(input_csv_file: str, model_path: str, learning_rate: float, epochs:
     from pyfasttext import FastText
     import tempfile
     import os
+    import mlflow
     
     df = pd.read_csv(input_csv_file)
     
@@ -30,6 +32,12 @@ def fasttext(input_csv_file: str, model_path: str, learning_rate: float, epochs:
         with open(tmp_path, 'w') as f:
             for label, words in trigrams_set:
                 f.write('__label__{} {}\n'.format(label, ' '.join(words)))
-        model = FastText()
-        model.supervised(input=tmp_path, output=model_path, epoch=epochs, lr=learning_rate)
+                
+        with mlflow.start_run():
+            mlflow.log_param('learning_rate', learning_rate)
+            mlflow.log_param('number of epochs', epochs)
+            with open(run_uuid_file, 'w') as file_desc:
+                file_desc.write(mlflow.active_run().info.run_uuid)
+            model = FastText()
+            model.supervised(input=tmp_path, output=model_path, epoch=epochs, lr=learning_rate)
     
--- scripts/cmd/classify.sh
+++ scripts/cmd/classify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 pushd "$(git rev-parse --show-toplevel)"
 
-dvc run -d ./data/data_train_tokenized.csv  -o ./data/fasttext_model.bin ./scripts/python/classify --learning-rate 0.7 --epochs 10 --input-csv-file ./data/data_train_tokenized.csv  --model-path  ./data/fasttext_model
+dvc run -d ./data/data_train_tokenized.csv  -o ./data/fasttext_model.bin -o ./data/run_uuid.txt ./scripts/python/classify --learning-rate 0.7 --epochs 10 --input-csv-file ./data/data_train_tokenized.csv  --model-path  ./data/fasttext_model --run-uuid-file  ./data/run_uuid.txt
 
 
 popd
